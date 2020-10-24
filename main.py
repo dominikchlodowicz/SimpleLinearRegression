cbfce24b6bde52e13e0b39acd69209a32af76c0f
dominikchlodowicz <dominikchlodowicz@gmail.com>
2020-10-24 16:26:37 +0200

    Realise that equations were wrong

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_excel('./dane.xlsx')
 
-age_x = df['age X'].iloc[0:len(df['age X']) - 1].tolist()
+def linear_regression(x, y, xlsx_file, csv_file ):
+    df = pd.read_excel('./dane.xlsx')
 
-glucose_level_y = df['glucose level Y'].iloc[0:len(df['glucose level Y']) - 1].tolist()
+    age_x = df['age X'].iloc[0:len(df['age X']) - 1].tolist()
 
-sum_age_x = sum(age_x)
+    glucose_level_y = df['glucose level Y'].iloc[0:len(df['glucose level Y']) - 1].tolist()
 
-sum_glucose_level_y = sum(glucose_level_y)
+    sum_age_x = sum(age_x)
 
-sum_pow_age_x = sum([i**2 for i in age_x])
+    sum_glucose_level_y = sum(glucose_level_y)
 
-sum_pow_glucose_level_y = sum([i**2 for i in glucose_level_y])
+    sum_pow_age_x = sum([i**2 for i in age_x])
 
-sum_xy = sum(age_x[i] * glucose_level_y[i] for i in range(len(age_x)))
+    # sum_pow_glucose_level_y = sum([i**2 for i in glucose_level_y])
 
-n_of_elements = len(df['subject']) - 1
+    sum_xy = sum(age_x[i] * glucose_level_y[i] for i in range(len(age_x)))
 
-a = ((sum_glucose_level_y * sum_pow_age_x) - (sum_age_x * sum_xy)) / \
-    ((n_of_elements * sum_pow_age_x) - sum_age_x ** 2)
-b = ((n_of_elements * sum_xy) - (sum_age_x * sum_glucose_level_y)) / \
-    ((n_of_elements * sum_pow_age_x) - sum_age_x ** 2)
+    n_of_elements = len(df['subject']) - 1
 
-X = 26
+    a = ((sum_glucose_level_y * sum_pow_age_x) - (sum_age_x * sum_xy)) / \
+        ((n_of_elements * sum_pow_age_x) - sum_age_x ** 2)
+    b = ((n_of_elements * sum_xy) - (sum_age_x * sum_glucose_level_y)) / \
+        ((n_of_elements * sum_pow_age_x) - sum_age_x ** 2)
 
-Y = a + (b * X)
+    # Y = a + (b * X)
 
-print(Y)
+    print(age_x, glucose_level_y)
+
+    plt.plot(age_x, glucose_level_y)
+    plt.xlabel('Ages(x)')
+    plt.ylabel('Glucose level(y)')
+    plt.title('Glucose level and age regression')
+    plt.show()
+
