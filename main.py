7bfea7c8b00c0932a738540f0ad74cc575b91bfc
dominikchlodowicz <dominikchlodowicz@gmail.com>
2020-11-03 17:13:29 +0100

    Clean script
    TODO:
         - file compatibility
         - data cleaning algorithm

--- main.py
+++ main.py
@@ -35,48 +35,19 @@ def regression(domain, slope, y_intercept):
     return (slope * domain) + y_intercept
 
 
-def plot_handling(domain, regression, x_label, y_label):
-    plt.plot(domain[0], domain[1], "o")
-    plt.plot(domain[3], regression[0])
+def plot_handling(data, model, x_label, y_label, title):
+    plt.plot(data[0], data[1], "o")
+    plt.plot(data[2], regression(data[2], model[0], model[1]))
     plt.grid(True)
+    plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.show()
 
-def main(xlsx_file, X, Y, x_max, population_or_sample):
-    pass
 
-# def main(file, x_label, y_label, population_or_sample):
-#     data_handling()
-#
-#
-# def linear_regression(data, x_max, population_or_sample):
-#     x = data[0]
-#     y = data[1]
-#     avg_x = sum(x)/len(x)
-#     avg_y = sum(y)/len(y)
-#     pearson_r_numerator = sum([(x[i] - avg_x) * (y[i] - avg_y) for i in range(len(x))])
-#     pearson_r_divisor = m.sqrt(sum([(x[i] - avg_x) ** 2 for i in range(len(x))]) * sum([(y[i] - avg_y) ** 2 for i in range(len(y))]))
-#
-#     if population_or_sample == 'population':
-#         variance_x = m.sqrt(sum([(x[i] - avg_x) ** 2 for i in range(len(x))]) / len(x))
-#         variance_y = m.sqrt(sum([(y[i] - avg_y) ** 2 for i in range(len(y))]) / len(y))
-#     elif population_or_sample == 'sample':
-#         variance_x = m.sqrt(sum([(x[i] - avg_x) ** 2 for i in range(len(x))]) / len(x) - 1)
-#         variance_y = m.sqrt(sum([(y[i] - avg_y) ** 2 for i in range(len(y))]) / len(y) - 1)
-#     else:
-#         return 'Seems like there\'s no information about type of data, please supply information to ' \
-#                '(population_or_sample) parameter'
-#
-#     slope = (pearson_r_numerator / pearson_r_divisor) * (variance_y / variance_x)
-#     y_intercept = avg_y - (slope * avg_x)
-#     domain = np.array([min(x), x_max])
-#     plt.plot(x, y, "o")
-#     plt.plot(domain, regression(domain, slope, y_intercept))
-#     plt.grid(True)
-#     plt.xlabel(X)
-#     plt.ylabel(Y)
-#     plt.show()
+def main(xlsx_file, X, Y, x_max, population_or_sample, title):
+    data = data_handling(xlsx_file, X, Y, x_max)
+    plot_handling(data, linear_regression(data, population_or_sample), X, Y, title)
 
-#
-# linear_regression(data_handling('./data.xlsx', 'Age', 'Glucose level'), 100, 'sample')
+
+main('./data.xlsx', 'Age', 'Glucose level', 100, 'population', 'Glucose level compared to age of patient')
